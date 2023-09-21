d7ab5b0136f9ee40e7bee3fd56d7c26b268de990
Will Cygan <wcygan.io@gmail.com>
2023-09-21 01:01:30 -0500

    make the script one line

--- scripts/proto.sh
+++ scripts/proto.sh
@@ -1,7 +1,3 @@
 # Generate the javascript code from the proto files
 # Run this script from the root of the project
-protoc \
-    --proto_path=schemas/protos/ \
-    --js_out=import_style=commonjs,binary:twote-frontend/src/proto \
-    --grpc-web_out=import_style=commonjs,mode=grpcwebtext:twote-frontend/src/proto \
-    schemas/protos/*.proto
+protoc --proto_path=schemas/protos/ --js_out=import_style=commonjs,binary:twote-frontend/src/proto --grpc-web_out=import_style=commonjs,mode=grpcwebtext:twote-frontend/src/proto schemas/protos/*.proto
