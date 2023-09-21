714212fb4a367d196719a06ab639bb0cae37bffe
Will Cygan <32029716+wcygan@users.noreply.github.com>
2023-09-20 21:16:19 -0500

    Add accounts-backend service (#4)
    * Add accounts-backend
    
    * pool the login clients

--- twote-api/src/service/login.rs
+++ twote-api/src/service/login.rs
@@ -1,17 +1,33 @@
-use tonic::{Code, Request, Response, Status};
+use anyhow::Result;
+use tonic::transport::Channel;
+use tonic::{Request, Response, Status};
+use tub::Pool;
 
+use schemas::login::login_service_client::LoginServiceClient;
 use schemas::login::login_service_server::LoginService;
 use schemas::login::{LoginRequest, LoginResponse};
 
-pub struct LoginServiceImpl;
+pub struct LoginServiceImpl {
+    login_service_clients: Pool<LoginServiceClient<Channel>>,
+}
+
+impl LoginServiceImpl {
+    pub async fn new() -> Result<Self> {
+        let client = LoginServiceClient::connect("http://localhost:50052").await?;
+
+        Ok(LoginServiceImpl {
+            login_service_clients: Pool::from_initializer(10, || client.clone()),
+        })
+    }
+}
 
 #[tonic::async_trait]
 impl LoginService for LoginServiceImpl {
     async fn login(
         &self,
-        _request: Request<LoginRequest>,
+        request: Request<LoginRequest>,
     ) -> Result<Response<LoginResponse>, Status> {
-        let message = format!("not implemented. Sorry {}!", _request.into_inner().username);
-        Err(Status::new(Code::Aborted, message))
+        let mut login_service_client = self.login_service_clients.acquire().await;
+        login_service_client.login(request).await
     }
 }
