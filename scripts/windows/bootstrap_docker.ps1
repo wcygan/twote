dbbaebc900d90b89c6519713b5e68906713f5af5
Will Cygan <wcygan.io@gmail.com>
2023-09-23 09:59:48 -0500

    fix docker bootstrap scripts

--- scripts/windows/bootstrap_docker.ps1
+++ scripts/windows/bootstrap_docker.ps1
@@ -1,3 +1,5 @@
+docker-compose down
+
 docker pull lukemathwalker/cargo-chef:latest-rust-1.72
 
 docker pull node:16
@@ -7,5 +9,7 @@ docker build -f images/twote-javascript-runtime/Dockerfile -t twote-javascript-r
 docker build -f images/twote-rust-builder/Dockerfile -t twote-rust-builder .
 
 docker build -f images/twote-rust-runtime/Dockerfile -t twote-rust-runtime .
+
+docker-compose build
 
 docker-compose up -d
