e21908df6c953f4a86b3d42614c4c4f4a62553b1
Will Cygan <32029716+wcygan@users.noreply.github.com>
2023-09-23 20:03:33 -0500

    Resubmit https://github.com/wcygan/twote/pull/8 (#13)
    * convince envoy to set the x-request-id header
    
    * install tracing
    
    * fix log messages
    
    * get rid of request id code since it already exists without it
    
    * fmt
    
    * clippy
    
    * add a license
    
    * Improve Docker build speeds... (#8)
    
    * specifying all cargo.tomls
    
    * Dockerfile without copy . .
    
    * splitting the dockerfiles further

---
+++ scripts/windows/bootstrap_docker.ps1
@@ -0,0 +1,11 @@
+docker pull lukemathwalker/cargo-chef:latest-rust-1.72
+
+docker pull node:16
+
+docker build -f images/twote-javascript-runtime/Dockerfile -t twote-javascript-runtime .
+
+docker build -f images/twote-rust-builder/Dockerfile -t twote-rust-builder .
+
+docker build -f images/twote-rust-runtime/Dockerfile -t twote-rust-runtime .
+
+docker-compose up -d
