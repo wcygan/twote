ae36dffda6492c68132f5f1dfa9914da94bcc61b
Will Cygan <wcygan.io@gmail.com>
2023-09-25 23:05:59 -0500

    navigate to create account page

--- twote-frontend/src/components/LoginComponent.js
+++ twote-frontend/src/components/LoginComponent.js
@@ -1,10 +1,12 @@
 import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import { LoginRequest } from '../proto/account_pb.js';
 import { AccountServiceClient } from '../proto/account_grpc_web_pb.js';
 import { AUTH_TOKEN} from '../middleware/AuthInterceptor.js';
 import Cookies from 'js-cookie';
 
 function LoginComponent() {
+    const navigate = useNavigate();
     const [response, setResponse] = useState('');
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
@@ -28,6 +30,11 @@ function LoginComponent() {
         });
     };
 
+    // Function to navigate to /create-account route
+    const navigateToCreateAccount = () => {
+        navigate('/create-account');
+    }
+
     return (
         <div>
             <div>
@@ -46,7 +53,8 @@ function LoginComponent() {
                     onChange={(e) => setPassword(e.target.value)}
                 />
             </div>
-            <button onClick={sendRequest}>Send gRPC Request</button>
+            <button onClick={sendRequest}>Login</button>
+            <button onClick={navigateToCreateAccount}>Need an Account?</button> {/* Create Account button */}
             <p>Response: {response}</p>
         </div>
     );
