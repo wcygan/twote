129d03e01a71ee28085c719b374e77bd2b994cc2
Will Cygan <wcygan.io@gmail.com>
2023-09-25 22:21:53 -0500

    seeing status code in response interceptor

--- twote-frontend/src/middleware/AuthInterceptor.js
+++ twote-frontend/src/middleware/AuthInterceptor.js
@@ -3,17 +3,6 @@ import Cookies from 'js-cookie';
 require('grpc-web');
 export const AUTH_TOKEN = "authorization-token"
 
-// // The UnaryInterceptor interface is for the promise-based client.
-// class MyUnaryInterceptor {
-//     intercept(request, invoker) {
-//         return invoker(request).then(response => {
-//             console.log("hello got code" + response.getCode());
-//             return response;
-//         });
-//     }
-// }
-
-
 /**
  * A ClientReadableStream wrapper.
  *
@@ -28,21 +17,16 @@ const InterceptedStream = function (stream) {
 
 /** @override */
 InterceptedStream.prototype.on = function (eventType, callback) {
-    console.log("bababooy")
-    console.log(eventType)
-    if (eventType === 'data') {
-        console.log("foo")
+    if (eventType === 'error') {
         const newCallback = (response) => {
-            console.log("boob")
-            // Update the response message.
-            console.log("code is " + response)
-            // Pass along the updated response.
+            console.log("got " + eventType)
+            console.log(response)
+            console.log(response.code)
             callback(response);
         };
-        // Register the new callback.
+
         this.stream.on(eventType, newCallback);
     } else {
-        // You can also override 'status', 'end', and 'error' eventTypes.
         this.stream.on(eventType, callback);
     }
     return this;
@@ -58,55 +42,14 @@ InterceptedStream.prototype.cancel = function () {
  * @constructor
  * @implements {StreamInterceptor}
  */
-const TestStreamInterceptor = function() {};
+const TestStreamInterceptor = function () {
+};
 
 /** @override */
-TestStreamInterceptor.prototype.intercept = function(request, invoker) {
+TestStreamInterceptor.prototype.intercept = function (request, invoker) {
     return new InterceptedStream(invoker(request));
 };
 
-//
-// const UnauthenticatedResponseInterceptor = function() {
-//     this.intercept = function(request, invoker) {
-//         return invoker(request)
-//             .then((response) => {
-//                 console.log("got code" + response.getCode());
-//
-//                 const token = Cookies.get(AUTH_TOKEN);
-//                 console.log("adding token to request: " + token);
-//
-//                 return response;
-//             });
-//     };
-// };
-
-
-// /**
-//  * @constructor
-//  * @implements {UnaryInterceptor}
-//  */
-// const SimpleUnaryInterceptor = function() {};
-//
-// /** @override */
-// SimpleUnaryInterceptor.prototype.intercept = function(request, invoker) {
-//     // Update the request message before the RPC.
-//     const reqMsg = request.getRequestMessage();
-//     reqMsg.setMessage('[Intercept request]' + reqMsg.getMessage());
-//
-//     // After the RPC returns successfully, update the response.
-//     return invoker(request).then((response) => {
-//         // You can also do something with response metadata here.
-//         console.log(response.getMetadata());
-//
-//         console.log("foo")
-//
-//         // Update the response message.
-//         const responseMsg = response.getResponseMessage();
-//         responseMsg.setMessage('[Intercept response]' + responseMsg.getMessage());
-//
-//         return response;
-//     });
-// };
 
 class AuthInterceptor {
     constructor() {
@@ -125,7 +68,6 @@ const authInterceptor = new AuthInterceptor()
 
 export const authOptions = {
     // Add interceptors that are called on each request
-    unaryInterceptors: [authInterceptor, new TestStreamInterceptor()],
     streamInterceptors: [authInterceptor, new TestStreamInterceptor()],
 }
 
