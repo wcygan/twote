279a4f8081ff96adaf1ccbbd7a6cc2cfabb53b7e
Will Cygan <wcygan.io@gmail.com>
2023-09-27 00:12:52 -0500

    wire up the API with profiles-backend methods

--- twote-api/src/main.rs
+++ twote-api/src/main.rs
@@ -1,9 +1,11 @@
 use crate::service::hello::HelloServiceImpl;
 use crate::service::login::AccountServiceImpl;
+use crate::service::profile::ProfileServiceImpl;
 use common::middleware::authentication::AuthMiddleware;
 use common::Service::TwoteApi;
 use schemas::account::account_service_server::AccountServiceServer;
 use schemas::hello::hello_service_server::HelloServiceServer;
+use schemas::profile::profile_service_server::ProfileServiceServer;
 use tonic::transport::Server;
 
 mod service;
@@ -18,6 +20,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     // Create the services
     let login_service = AccountServiceServer::new(AccountServiceImpl);
     let hello_service = HelloServiceServer::new(HelloServiceImpl);
+    let profile_service = ProfileServiceServer::new(ProfileServiceImpl);
     let (_, health_service) = tonic_health::server::health_reporter();
 
     // Start the server
@@ -27,6 +30,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         .layer(AuthMiddleware::default())
         .add_service(health_service)
         .add_service(login_service)
+        .add_service(profile_service)
         .add_service(hello_service)
         .serve(addr)
         .await?;
