800659605cd62d068249beaa87953142c5fe952d
Will Cygan <wcygan.io@gmail.com>
2023-10-08 19:43:37 -0500

    get rid of middleware module

--- common/src/lib.rs
+++ common/src/lib.rs
@@ -4,7 +4,7 @@ mod db;
 mod service;
 pub use db::{MongoCollection, MongoDB};
 pub use service::Service;
-pub mod middleware;
+pub mod authentication;
 
 pub fn spawn_blocking_with_tracing<F, R>(f: F) -> JoinHandle<R>
 where
